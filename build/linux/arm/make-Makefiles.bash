d5fe703b212c32dd94fac37c0d15e66c1061e30d
Herman Chen <herman.chen@rock-chips.com>
2020-04-23 11:51:36 +0800

    [WIP]: patch for rv1109 sdk
    Change-Id: I50f48479f7ce0d5c523c3836e1870701bd46e2df
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>

--- build/linux/arm/make-Makefiles.bash
+++ build/linux/arm/make-Makefiles.bash
@@ -5,24 +5,29 @@ set +e
 
 MPP_PWD=`pwd`
 MPP_TOP=${MPP_PWD}/../../..
+SDK_TOOLCHAIN_FILE=/home/herman/work/rv1126/buildroot/output/rockchip_rv1126_rv1109/host/share/buildroot/toolchainfile.cmake
 
 # toolchain detection
-check_cmd(){
-    "$@" >> /dev/null 2>&1
-}
-check_system_arm_linux_gcc(){
-    check_cmd arm-linux-gcc -v
-}
-
-check_system_arm_linux_gcc
-if [ $? -eq 127 ];then
-    MPP_TOOLCHAIN=${MPP_TOP}/../prebuilts/toolschain/usr/bin
-    export PATH=$PATH:${MPP_TOOLCHAIN}
-fi
+#check_cmd(){
+#    "$@" >> /dev/null 2>&1
+#}
+#check_system_arm_linux_gcc(){
+#    check_cmd arm-linux-gcc -v
+#}
+#
+#check_system_arm_linux_gcc
+#if [ $? -eq 127 ];then
+#    MPP_TOOLCHAIN=${MPP_TOP}/../prebuilts/toolschain/usr/bin
+#    export PATH=$PATH:${MPP_TOOLCHAIN}
+#fi
+#      -DCMAKE_TOOLCHAIN_FILE=./arm.linux.cross.cmake \
+#      -DRKPLATFORM=ON \
 
 # generate Makefile
-cmake -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_TOOLCHAIN_FILE=./arm.linux.cross.cmake \
-      -DRKPLATFORM=ON \
+cmake -DCMAKE_BUILD_TYPE=Debug \
+      -DCMAKE_TOOLCHAIN_FILE=${SDK_TOOLCHAIN_FILE} \
+      -DHAVE_DRM=ON \
       -G "Unix Makefiles" \
       ${MPP_TOP}
+
+cmake --build . -j
